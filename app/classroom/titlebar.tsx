aec82e520d5a05f23b8fd1ee0d28bd20f9d25da8
Vico Pratama <135086306+guguboo@users.noreply.github.com>
2025-04-13 12:03:32 +0700

    merge-revision
    merge sblmnya salah, direvisi

--- app/classroom/titlebar.tsx
+++ app/classroom/titlebar.tsx
@@ -82,32 +82,32 @@ export default function Titlebar({ type }: { type: string }) {
         )
     }
 
-    return (
-        <header className="titlebar" suppressHydrationWarning>
-            <img className="icon" src="/favicon.ico"/>
-            <span className="name">Codi</span>
-            <div className="controls">
-                <div className="content">
-                    <div className="navigation"> 
-                        <button className="plain icon">&#xef52;</button>
-                        <button className="plain icon">&#xef59;</button>
-                    </div>
-                    <label className="InputContainer_Textbox" >
-                        <input id="Input_GlobalSearch" placeholder="Cari" autoComplete="off" type="search" ad-abortstyle="true"/>
-                    </label>
-                    <div className="accessibility"> 
-                        <button className="accent" onClick={readPage}>
-                            <span className="icon">&#xfb58;</span>
-                            <span>Bacakan</span>
-                        </button>
-                    <div className="accessibility" suppressHydrationWarning> 
-                        { accessibility(type) }
-                    </div>
-                    <div className="profile" id="Image_Profile">
-                        <img src="/avatar.webp"/>
+    const element = function(type: string)
+    {
+        return (
+            <header className="titlebar" suppressHydrationWarning>
+                <img className="icon" src="/favicon.ico"/>
+                <span className="name">Codi</span>
+                <div className="controls">
+                    <div className="content">
+                        <div className="navigation"> 
+                            <button className="plain icon">&#xef52;</button>
+                            <button className="plain icon">&#xef59;</button>
+                        </div>
+                        <label className="InputContainer_Textbox" >
+                            <input id="Input_GlobalSearch" placeholder="Cari" autoComplete="off" type="search" ad-abortstyle="true"/>
+                        </label>
+                        <div className="accessibility" suppressHydrationWarning> 
+                            { accessibility(type) }
+                        </div>
+                        <div className="profile" id="Image_Profile">
+                            <img src="/avatar.webp"/>
+                        </div>
                     </div>
                 </div>
-            </div>
         </header>  
-    )
+        );
+    }
+
+    return element(type)
 }
